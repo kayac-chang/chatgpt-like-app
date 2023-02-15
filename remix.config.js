9406d3429127f48b53fdd7882f5f4cf2128aa185
kayac-chang <egg734631@gmail.com>
2023-02-15 22:41:27 +0800

    feat: 🎸 update

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverDependenciesToBundle: [/@sindresorhus/],
+  serverDependenciesToBundle: [/@sindresorhus/, /zodrq/],
 };
